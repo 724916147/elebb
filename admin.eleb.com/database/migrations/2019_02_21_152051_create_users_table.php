3a5d898b2672c19492fba0ecdda1318c4616ff28
111 <111>
2019-02-21 15:37:26 +0800

    平台数据库 MODel

--- admin.eleb.com/database/migrations/2019_02_21_152051_create_users_table.php
+++ admin.eleb.com/database/migrations/2019_02_21_152051_create_users_table.php
@@ -16,9 +16,11 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->string('password');
-            $table->rememberToken();
+            $table->string('remember_token');
+            $table->integer('status');
+            $table->integer('shp_id');
             $table->timestamps();
         });
     }
