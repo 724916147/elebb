130e51579d49191f85d6e291d33c5333fb915e83
111 <111>
2019-02-22 08:55:38 +0800

    商户分类 商户列表

--- admin.eleb.com/app/Models/ShopCategory.php
+++ admin.eleb.com/app/Models/ShopCategory.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class ShopCategory extends Model
 {
     //
+    protected $fillable=['name','img','status'];
+    public function img()
+    {
+        return $this->img?Storage::url($this->img):'/image/hend.jpg';
+    }
 }
